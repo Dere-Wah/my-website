c2d70d652c52841ba0917250b9561b25a2e23732
DereWah <daviloca03@gmail.com>
2024-08-24 14:40:46 +0200

    first commit

---
+++ src/components/AgeDisplay.jsx
@@ -0,0 +1,67 @@
+import { useState, useEffect } from 'react';
+
+export default function AgeDisplay(){
+    const [type, setType] = useState("years");
+    const [unixTime, setUnixTime] = useState(Date.now() / 1000);
+
+    /**
+     * Set up an interval to update the time every second
+     * 
+     * Returns a function to clear the interval when the component unmounts
+     */
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setUnixTime(Date.now() / 1000);
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    // Only run this effect once, on mount
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+
+    let types = ["seconds", "mcdays", "years"];
+
+    function handleClick() {
+        let curr = type;
+        while(curr == type){
+            curr = types[Math.floor(Math.random()*(types.length))]
+        }
+        setType(curr);
+    }
+
+    const birthDateUnix = new Date('2003-08-29').getTime() / 1000;
+    const currentUnix = unixTime;
+    const age = Math.floor(currentUnix - birthDateUnix);
+
+    let display_number = null;
+    let display_unit = null;
+    let display_icon = null;
+
+    switch(type){
+        case "seconds":
+            display_number = age.toString();
+            display_unit = " seconds";
+            display_icon = "../NotoHourglassNotDone.svg";
+            break;
+        case "mcdays":
+            display_number = Math.floor(age*1000 / 1200.0)/1000 ;
+            display_unit = " Minecraft days"
+            display_icon = "../NotoPick.svg"
+            break;
+        case "years":
+            display_number = Math.floor((age / (24 * 60 * 60 * 365.25)));
+            display_unit = " years"
+            display_icon = "../NotoBirthdayCake.svg";
+            break;           
+    }
+
+    return(
+        <button onClick={handleClick} class="inline-flex align-center border border-ebony-clay-100/20 bg-ebony-clay-950/50 rounded-md px-1 font-semibold hover:underline hover:cursor-pointer">
+            {display_icon != null && <img src={display_icon} class="h-6 px-1 size-fit"></img>}
+            <span>{display_number.toString() + display_unit}</span>
+        </button>
+    )
+}
---
+++ tailwind.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			'ebony-clay': {
+				'50': '#f2f6fc',
+				'100': '#e1ebf8',
+				'200': '#cbdcf2',
+				'300': '#a6c6ea',
+				'400': '#7ca8de',
+				'500': '#5d8ad4',
+				'600': '#4971c7',
+				'700': '#3f5eb6',
+				'800': '#394e94',
+				'900': '#324476',
+				'950': '#1f2742',
+			},
+		},
+		extend: {},
+	},
+	plugins: [],
+}
