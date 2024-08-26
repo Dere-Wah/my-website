1859c5eaa3035a719215d2316639c92d6302e12c
DereWah <daviloca03@gmail.com>
2024-08-26 11:24:57 +0200

    fix: padding in blogposts. feat: add back to homepage HomeButton feat: add links at end of blogposts automatically

--- src/components/AgeDisplay.jsx
+++ src/components/AgeDisplay.jsx
@@ -59,8 +59,8 @@ export default function AgeDisplay(){
     }
 
     return(
-        <button onClick={handleClick} class="inline-flex items-center border border-ebony-clay-100/20 bg-ebony-clay-950/50 rounded-md px-1 h-6 font-semibold hover:underline hover:cursor-pointer">
-            {display_icon != null && <img src={display_icon} class="h-2/3 pe-1"></img>}
+        <button onClick={handleClick} className="inline-flex items-center border border-ebony-clay-100/20 bg-ebony-clay-950/50 rounded-md px-1 h-6 font-semibold hover:underline hover:cursor-pointer">
+            {display_icon != null && <img src={display_icon} className="h-2/3 pe-1"></img>}
             <span>{display_number.toString() + display_unit}</span>
         </button>
     )
